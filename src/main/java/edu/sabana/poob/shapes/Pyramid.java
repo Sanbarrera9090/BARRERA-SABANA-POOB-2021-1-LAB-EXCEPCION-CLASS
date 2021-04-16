c95e9acf577f2f7160b462880da649dacce287ee
Santiago Barrera <santiagobasa@unisabana.edu.co>
2021-04-16 16:07:07 -0500

    commit 1

---
+++ src/main/java/edu/sabana/poob/shapes/Pyramid.java
@@ -0,0 +1,56 @@
+package edu.sabana.poob.shapes;
+
+public class Pyramid extends Triangle implements GeometricShape3D {
+
+    private double height = 1.0;
+
+    public Pyramid() {
+    }
+
+    public Pyramid(String color) throws ShapeException {
+
+        if (height <= 0)
+            throw new ShapeException(ShapeException.BAD_DIMENSION_SIDE);
+
+
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+
+
+    }
+
+    public Pyramid(double side1, double side2, double side3, double height) {
+        super(side1, side2, side3);
+        this.height = height;
+    }
+
+    public Pyramid(String color, double side1, double side2, double side3, double height) {
+        super(color, side1, side2, side3);
+        this.height = height;
+    }
+
+    public double getArea() {
+        return Math.pow(side1, 2) + Math.pow(height, 2);
+    }
+
+    public double getPerimeter() {
+        return side1 * side2 * side3;
+    }
+
+    @Override
+    public double getVolume() {
+        return (side1 * side2) * height / 3;
+    }
+
+    @Override
+    public double getSuperficialArea() {
+        return height + side1;
+    }
+
+    @Override
+    public String toStringGeometricShape3D() {
+        return "This is a Pyramid";
+    }
+
+}
---
+++ src/test/java/edu/sabana/poob/shapes/CylinderTest.java
@@ -0,0 +1,84 @@
+package edu.sabana.poob.shapes;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CylinderTest {
+
+    private static Cylinder c1;
+    private static Cylinder c2;
+    private static Cylinder c3;
+
+    @BeforeAll
+    public static void setUp() throws ShapeException {
+        c1 = new Cylinder();
+        c2 = new Cylinder(14.5, 20.5);
+        c3 = new Cylinder("red", 23.5, 42.1);
+    }
+
+    @Test
+    public void shouldCalculateArea() {
+        assertEquals(3, (int) c1.getArea());
+        assertEquals(660, (int) c2.getArea());
+        assertEquals(1734, (int) c3.getArea());
+    }
+
+    @Test
+    public void shouldCalculatePerimeter() {
+
+        assertEquals(6, (int) c1.getPerimeter());
+        assertEquals(91, (int) c2.getPerimeter());
+        assertEquals(147, (int) c3.getPerimeter());
+    }
+
+    @Test
+    public void shouldCalculateDiameter() {
+
+        assertEquals(2, (int) c1.getDiameter());
+        assertEquals(29, (int) c2.getDiameter());
+        assertEquals(47, (int) c3.getDiameter());
+    }
+
+    @Test
+    public void shouldCalculateVolume() {
+
+        assertEquals(3, (int) c1.getVolume());
+        assertEquals(660, (int) c2.getVolume());
+        assertEquals(1734, (int) c3.getVolume());
+    }
+
+    @Test
+    public void shouldCalculateSuperficialArea() {
+
+        assertEquals(39, (int) c1.getSuperficialArea());
+        assertEquals(120354, (int) c2.getSuperficialArea());
+        assertEquals(512345, (int) c3.getSuperficialArea());
+    }
+    @Test
+    public void shouldPrintCylinder() {
+
+        assertTrue(c1.toStringGeometricShape3D().contains("This is a Cylinder"));
+        assertTrue(c2.toStringGeometricShape3D().contains("This is a Cylinder"));
+        assertTrue(c3.toStringGeometricShape3D().contains("This is a Cylinder"));
+    }
+
+    @Test
+    public void shouldNorCreateCircleWithRadiusNegative() {
+
+        try {
+            new Cylinder(-1);
+        } catch (ShapeException e) {
+            assertEquals(ShapeException.BAD_DIMENSION_SIDE, e.getMessage());
+        }
+    }
+        @Test
+        public void shouldNorCreateCircleWithRadiusZero() {
+
+            Exception e = assertThrows(ShapeException.class, () -> new Cylinder(0));
+            assertEquals(ShapeException.BAD_DIMENSION_SIDE, e.getMessage());
+        }
+
+
+}
